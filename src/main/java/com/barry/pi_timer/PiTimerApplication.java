7c45a055a35a40b93cff7134336740a39c826e4c
Barry Tarlton <TARLTOB1@nationwide.com>
2017-09-22 19:21:59 -0400

    initial commit of spring boot starter app

---
+++ src/main/java/com/barry/pi_timer/PiTimerApplication.java
@@ -0,0 +1,12 @@
+package com.barry.pi_timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiTimerApplication.class, args);
+	}
+}
